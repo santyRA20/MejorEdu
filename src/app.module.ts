a1a380d793ceeacd8821b5a170589f58d1982110
santyRA20 <santiagoroa2020@gmail.com>
2025-06-25 17:31:53 -0600

    Logica de Autenticacion de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
-import { Module } from '@nestjs/common';
+/*import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
+})
+export class AppModule {}*/
+
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { PrismaModule } from 'prisma/prisma.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    UserModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async login(username: string, password: string) {
+    if (username !== 'admin' || password !== 'admin') {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { username };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
